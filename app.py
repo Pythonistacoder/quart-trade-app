e10a083b17820a049f887a741dd90cad840c549c
Kunal Dey <pythonistacoder@gmail.com>
2023-01-21 01:00:55 +0530

    changed app from fn to var for hypercorn

--- app.py
+++ app.py
@@ -6,39 +6,33 @@ from service.background_task import background_task
 from utils.square_off import sqaure_off
 
 
-def main_app():
-    app = Quart(__name__)
-    app.config["PROPAGATE_EXCEPTIONS"] = True
-    app = cors(app, allow_origin="*")
 
-    @app.get("/start")
-    async def start_process():
-        try:
-            app.add_background_task(background_task)
-            return {"message":"Background process started"}
-        except:
-            return {"message":"Kindly login first"}
+app = Quart(__name__)
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app = cors(app, allow_origin="*")
+@app.get("/start")
+async def start_process():
+    try:
+        app.add_background_task(background_task)
+        return {"message":"Background process started"}
+    except:
+        return {"message":"Kindly login first"}
+@app.route("/stop")
+async def stop_background_tasks():
+    """
+        On being deployed if we need to manually stop the background task then
+        this route is used
+    """
+    for task in app.background_tasks:
+        task.cancel()
+    sqaure_off()
+    return {"message":"All task cancelled"}
+resource_list:Blueprint=[login]
+for resource in resource_list:
+    app.register_blueprint(blueprint=resource)
 
-    @app.route("/stop")
-    async def stop_background_tasks():
-        """
-            On being deployed if we need to manually stop the background task then
-            this route is used
-        """
-
-        for task in app.background_tasks:
-            task.cancel()
-        sqaure_off()
-        return {"message":"All task cancelled"}
-
-    resource_list:Blueprint=[login]
-
-    for resource in resource_list:
-        app.register_blueprint(blueprint=resource)
-
-    return app
 
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0',port=80)
-    main_app().run(port=8080)
+    app().run(port=8080)
